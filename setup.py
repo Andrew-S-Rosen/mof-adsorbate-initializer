0b486cce7e0f9c97219cec5b2f9f03dc871fd671
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-04-18 16:58:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='MAI',
       description='Python code to add adsorbates to MOFs',
       author='Andrew S. Rosen',
       author_email='rosen@u.northwestern.edu',
-      url='https://github.com/arosen93/mof-adsorbate-addition',
+      url='https://github.com/arosen93/mof-adsorbate-initializer',
       requires_python='>=3.6.0',
       packages=find_packages(),
       license='MIT'
