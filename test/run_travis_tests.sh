d85c5e1e31bbe2f28c4cde1dee305eafe6a01dcf
Andrew Rosen <rosen@u.northwestern.edu>
2018-04-17 20:26:57 -0500

    Fix path to TRAVIS test

--- test/run_travis_tests.sh
+++ test/run_travis_tests.sh
@@ -2,7 +2,7 @@
 
 # run_travis_tests.sh: Run the tests for Travis CI
 
-python ../examples/add_O.py &&
-python ../examples/add_H.py &&
-python ../examples/add_CH4.py &&
+python examples/add_O.py &&
+python examples/add_H.py &&
+python examples/add_CH4.py &&
 python analyze_test.py
