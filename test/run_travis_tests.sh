2f1d64a70b4ae167ed7548cdbf424eb5b7bd3e57
Andrew Rosen <rosen@u.northwestern.edu>
2018-09-22 19:57:44 -0500

    Try again with TRAVIS

--- test/run_travis_tests.sh
+++ test/run_travis_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # run_travis_tests.sh: Run the tests for Travis CI
-
-python examples/add_O.py &&
-python examples/add_O_OMS_omd.py &&
-python examples/add_O_OMS_zeo.py &&
-python examples/add_CH4_PEG_ASCII.py &&
+cd examples
+python add_O.py &&
+python add_O_OMS_omd.py &&
+python add_O_OMS_zeo.py &&
+python add_CH4_PEG_ASCII.py &&
+cd ../
 python test/analyze_test.py
