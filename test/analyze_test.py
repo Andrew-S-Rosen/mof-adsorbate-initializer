b938e6a4383a5e4bdf7728deb875979123a45090
Andrew Rosen <rosen@u.northwestern.edu>
2018-09-22 20:21:16 -0500

    Apparently TRAVIS was working fine...

--- test/analyze_test.py
+++ test/analyze_test.py
@@ -4,14 +4,11 @@ import numpy as np
 
 success_basepath = os.path.join('test','success')
 test_basepath = os.path.join('examples','new_mofs')
-print(os.getcwd(),success_basepath,test_basepath)
 jobs = ['add_O','add_O_OMS_omd','add_O_OMS_zeo','add_CH4_PEG_ASCII']
 tol = 1E-3
 for job in jobs:
 	success_path = os.path.join(success_basepath,job)
-	print(success_path)
 	for file in os.listdir(success_path):
-		print(file)
 		if '.cif' in file:
 			mof_test = read(os.path.join(test_basepath,file))
 			mof_real = read(os.path.join(success_path,file))
@@ -24,4 +21,6 @@ for job in jobs:
 				print('Difference:')
 				print(diff)
 				raise ValueError('Error with: '+file)
+			else:
+				print('SUCCESS for '+job)
 			break
