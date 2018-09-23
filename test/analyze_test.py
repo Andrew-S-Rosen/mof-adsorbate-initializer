16ce2582dfd74e15aa4217e7db48b6bb170850d1
Andrew Rosen <rosen@u.northwestern.edu>
2018-09-22 20:10:25 -0500

    Fix TRAVIS test
    I think I fixed it finally...

--- test/analyze_test.py
+++ test/analyze_test.py
@@ -8,19 +8,18 @@ print(os.getcwd(),success_basepath,test_basepath)
 jobs = ['add_O','add_O_OMS_omd','add_O_OMS_zeo','add_CH4_PEG_ASCII']
 tol = 1E-3
 for job in jobs:
-	path = os.path.join(test_basepath,job)
 	success_path = os.path.join(success_basepath,job)
 	for file in os.listdir(success_path):
-		if not os.path.isfile(os.path.join(path,file)) or '.cif' not in file:
-			continue
-		mof_test = read(os.path.join(path,file))
-		mof_real = read(os.path.join(success_path,file))
-		diff = np.abs(mof_real.get_positions()-mof_test.get_positions())
-		if np.sum(diff >= tol) != 0:
-			print('MOF (real):')
-			print(mof_real.get_positions())
-			print('MOF (test):')
-			print(mof_test.get_positions())
-			print('Difference:')
-			print(diff)
-			raise ValueError('Error with: '+file)
+		if '.cif' in file:
+			mof_test = read(os.path.join(test_basepath,file))
+			mof_real = read(os.path.join(success_path,file))
+			diff = np.abs(mof_real.get_positions()-mof_test.get_positions())
+			if np.sum(diff >= tol) != 0:
+				print('MOF (real):')
+				print(mof_real.get_positions())
+				print('MOF (test):')
+				print(mof_test.get_positions())
+				print('Difference:')
+				print(diff)
+				raise ValueError('Error with: '+file)
+			break
