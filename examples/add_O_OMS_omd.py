20e21fd7a66e3e9657c0d314132f0097e499222c
Andrew Rosen <rosen@u.northwestern.edu>
2018-09-25 15:05:06 -0500

    Remove example files

--- examples/add_O_OMS_omd.py
+++ examples/add_O_OMS_omd.py
@@ -2,13 +2,13 @@ import os
 from omsdetector import MofCollection
 from mai.adsorbate_constructor import adsorbate_constructor
 
-mofs_path = 'example_MOF_folder' #path to folder of CIFs
-analysis_path = mofs_path #path to store the OMS results
+mofs_path = 'example_MOFs' #path to folder of CIFs
+oms_data_path = mofs_path #path to store the OMS results
 ads_species = 'O' #adsorbate species
 bond_length = 2.0 #desired distance between OMS and ads_species
 
 #Run the Open Metal Detector
-mof_coll = MofCollection.from_folder(collection_folder=mofs_path,analysis_folder=analysis_path)
+mof_coll = MofCollection.from_folder(collection_folder=mofs_path,analysis_folder=mofs_path)
 mof_coll.analyse_mofs()
 
 #add adsorbate for every CIF in mofs_path
