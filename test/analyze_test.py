9a100764cecc9525e2a6a169eb00233e709b54da
Andrew Rosen <rosen@u.northwestern.edu>
2018-12-19 20:45:35 -0500

    More robust test cases for TRAVIS

--- test/analyze_test.py
+++ test/analyze_test.py
@@ -1,25 +1,22 @@
 import os
-from ase.io import read
+from pymatgen.io.cif import CifParser
 import numpy as np
+from pymatgen.analysis.structure_matcher import StructureMatcher
 
 success_basepath = os.path.join('test','success')
 test_basepath = os.path.join('examples','new_mofs')
 jobs = ['add_O','add_O2','add_N2O','add_H2O','add_CH4_PEG','add_O_auto']
-tol = 1E-3
+tol = 1E-6
 for job in jobs:
 	success_path = os.path.join(success_basepath,job)
 	for file in os.listdir(success_path):
 		if '.cif' in file:
-			mof_test = read(os.path.join(test_basepath,file))
-			mof_real = read(os.path.join(success_path,file))
-			diff = np.abs(mof_real.get_positions()-mof_test.get_positions())
-			if np.sum(diff >= tol) != 0:
-				print('MOF (real):')
-				print(mof_real.get_positions())
-				print('MOF (test):')
-				print(mof_test.get_positions())
-				print('Difference:')
-				print(diff)
+			mof_test = CifParser(os.path.join(test_basepath,file)).get_structures(primitive=False)[0]
+			mof_real = CifParser(os.path.join(success_path,file)).get_structures(primitive=False)[0]
+			sm = StructureMatcher(primitive_cell=False)
+			rms = sm.get_rms_dist(mof_test,mof_real)
+			if rms > tol:
+				print(rms)
 				raise ValueError('Error with: '+file)
 			else:
 				print('SUCCESS for '+job)
