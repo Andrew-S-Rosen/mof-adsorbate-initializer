1355f930719ccb8e57ece202fa8832c4b2921968
Andrew Rosen <rosen@u.northwestern.edu>
2018-12-19 20:51:29 -0500

    Finally fix TRAVIS

--- test/analyze_test.py
+++ test/analyze_test.py
@@ -1,12 +1,11 @@
 import os
 from pymatgen.io.cif import CifParser
-import numpy as np
 from pymatgen.analysis.structure_matcher import StructureMatcher
 
 success_basepath = os.path.join('test','success')
 test_basepath = os.path.join('examples','new_mofs')
 jobs = ['add_O','add_O2','add_N2O','add_H2O','add_CH4_PEG','add_O_auto']
-tol = 1E-6
+tol = 1E-4
 for job in jobs:
 	success_path = os.path.join(success_basepath,job)
 	for file in os.listdir(success_path):
@@ -15,8 +14,8 @@ for job in jobs:
 			mof_real = CifParser(os.path.join(success_path,file)).get_structures(primitive=False)[0]
 			sm = StructureMatcher(primitive_cell=False)
 			rms = sm.get_rms_dist(mof_test,mof_real)
-			if rms > tol:
-				print(rms)
+			if not rms or rms[0] > tol:
+				print(rms[0])
 				raise ValueError('Error with: '+file)
 			else:
 				print('SUCCESS for '+job)
